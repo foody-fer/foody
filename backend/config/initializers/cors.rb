2f1e235b338d15ca9dda7d8cc37aa6ee780d99d3
root <root@DESKTOP-EQI04R0.>
2024-12-16 21:46:38 +0100

    Your commit message here

---
+++ backend/config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3000' # Replace '*' with specific origins if needed, e.g., 'example.com'
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
