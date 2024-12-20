847061c1e798c6de62ead50f26d47e04600584b1
luka-duretic <luka.duretic@fer.unizg.hr>
2024-12-20 12:41:15 +0100

    izrada komponenti post i comment

---
+++ web/app/components/Feed.tsx
@@ -0,0 +1,17 @@
+import Post from "./Post";
+
+export default function Feed(){
+    const list = [['Pero Peric', 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=800', 121, 58],
+        ['Vedran Oblakovic', 'https://images.pexels.com/photos/793785/pexels-photo-793785.jpeg?auto=compress&cs=tinysrgb&w=800', 1008, 236],
+        ['Ivica Ivic', 'https://images.pexels.com/photos/1640776/pexels-photo-1640776.jpeg?auto=compress&cs=tinysrgb&w=800', 22, 8],
+        ['Vid Vidic', 'https://images.pexels.com/photos/1346347/pexels-photo-1346347.jpeg?auto=compress&cs=tinysrgb&w=800', 78, 25],
+    ];
+
+    return (
+        <div className="w-[36%] h-auto bg-navbarColor rounded-lg flex flex-col justify-center items-center pt-3">
+            {list.map((item) => (
+                <Post name={item[0]} pic={item[1]} like={item[2]} com={item[3]} key={item[0]} />
+            ))}
+        </div>
+    );
+}
--- web/app/components/Post.tsx
+++ web/app/components/Post.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Comments from "./Comments";
 
-export default function Post(){
+export default function Post({name, pic, like, com} : any){
     const [likes, setLikes] = useState(false);
     const [comm, setComm] = useState(true);
     const [saves, setSaves] = useState(false);
-    const [likeNum, setLikeNum] = useState(121);
-    const [commNum, setCommNum] = useState(58);
+    const [likeNum, setLikeNum] = useState(like);
+    const [commNum, setCommNum] = useState(com);
 
     
     const handleClick1 = () => {
@@ -36,24 +37,24 @@ export default function Post(){
         picture = './images/like.png'
 
     return (
-        <div className="h-auto bg-gray-100 flex flex-col gap-4 mb-4 p-2 rounded-lg">
+        <div className="h-auto w-[93%] bg-gray-100 flex flex-col gap-3 mb-4 p-2 rounded-lg">
             {/* USER */}
             <div className="flex flex-row justify-between">
                 <div className="flex flex-row gap-2">
-                    <img src="./images/user_icon.jpg" alt="user pic" height={40} width={40} className="rounded-full"/>
-                    <span className="text-textColor mt-2">Pero Peric</span>
+                    <img src="./images/user_icon.jpg" alt="user pic" height={40} width={40} className="rounded-full flex-1"/>
+                    <span className="text-textColor mt-2">{name}</span>
                 </div>
                 <img src="./images/more.svg" alt="" height={16} width={16}/>
             </div>
             {/* CONTENT */}
             <div className="flex flex-col gap-2">
-                <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=800" alt="food" className="w-full rounded-md"/>
+                <img src={pic} alt="food" className="w-full rounded-md"/>
                 <span className="text-textColor"> Ovdje ide tekst post-a sdass sd as da s ss ssssadfas  fasasfasfa  asf asf sf as fasfsafs</span>
             </div>
             {/* INTERACTION */}
             <div className="flex flex-row justify-between text-xs">
                 <div className="flex flex-row gap-2">
-                    <div className="text-textColor flex flex-row  bg-white rounded-full p-1">
+                    <div className={"flex flex-row  bg-white rounded-full p-1 transition duration-300 "+(likes ? 'text-resedaGreen font-semibold' : ' text-textColor')}>
                         <img src={picture} alt="like" onClick={handleClick1} className={`transition duration-500 cursor-pointer mt-[0.1rem] ${css}`}/>
                         <span className="mr-1 cursor-pointer mt-[0.1rem]" onClick={handleClick1}>Like |</span>
                         <span className="pr-2 mt-[0.1rem]">{likeNum}</span>
@@ -69,6 +70,10 @@ export default function Post(){
                     <span className="pr-2 pl-1 cursor-pointer" onClick={handleClick3}>Save</span>
                 </div>
             </div>
+            {/* COMMENTS */}
+            {com && (
+                <Comments bool={comm} />
+            )}
         </div>
     );
 }
