4a8155b821ae0157a121c4a9a0f78902c3c5d651
ZuanovicLuka <luka.zuanovic@fer.unizg.hr>
2024-12-20 18:23:53 +0100

    Napravljen sidebar i promjena nekoliko stvari u kodu

---
+++ web/app/(auth)/homepage/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Feed from "../../components/Feed";
+
+export default function Homepage() {
+    
+    return (
+      <div className="text-textColor bg-backgroundGreen">
+          <div className="flex ml-60 pt-3">
+            <Feed/>
+          </div>
+      </div>
+    );
+}  
---
+++ web/app/(auth)/layout.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import React from 'react';
+import { IoHomeOutline, IoBarChartOutline, IoPersonOutline, IoHome, IoBarChart, IoPerson } from "react-icons/io5";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen bg-backgroundGreen">
+      <aside className="w-[13%] bg-navbarColor p-2 fixed h-full ml-44 mt-3 rounded-lg">
+        <nav>
+            <img src="/images/logo.png" alt="Logo" className='mt-10 mb-28'/>
+
+            <div className='flex flex-col items-center justify-center mb-24'>
+              <Link href={'/homepage'} className="group flex flex-col items-center justify-center">
+                <IoHomeOutline size={50} color="white" className="group-hover:hidden" />
+                <IoHome size={50} className="hidden group-hover:block text-resedaGreen" />
+                <p className="text-center text-[#ffffff] group-hover:text-resedaGreen">Home</p>
+              </Link>
+            </div>
+
+            <div className='flex flex-col items-center justify-center mb-24'>
+              <Link href={'/progress'} className="group flex flex-col items-center justify-center">
+                <IoBarChartOutline size={50} color="white" className="group-hover:hidden" />
+                <IoBarChart size={50} className="hidden group-hover:block text-resedaGreen" />
+                <p className="text-center text-[#ffffff] group-hover:text-resedaGreen">Progress</p>
+              </Link>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              <Link href={'/profile'} className="group flex flex-col items-center justify-center">
+                <IoPersonOutline size={50} color="white" className="group-hover:hidden" />
+                <IoPerson size={50} className="hidden group-hover:block text-resedaGreen" />
+                <p className="text-center text-[#ffffff] group-hover:text-resedaGreen">Profile</p>
+              </Link>
+            </div>
+        </nav>
+      </aside>
+
+      <main className='w-full ml-[23%] overflow-y-auto'>
+        {children}
+      </main>
+    </div>
+  );
+}
