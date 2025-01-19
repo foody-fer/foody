6938d46cb210033840b56cbf7088942f5400a900
lovro-misak <lovro.misak@fer.unizg.hr>
2025-01-19 10:08:24 +0100

    Spajanje mobile Profile s backendom
    Početna verzija spajanja Profile screena s backendom, potrebno je još riješiti slanje podataka za ažuriranje profila

--- mobile/app/(tabs)/Profile.tsx
+++ mobile/app/(tabs)/Profile.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Avatar,
   AvatarFallbackText,
@@ -7,14 +8,86 @@ import { HStack } from "@/components/ui/hstack";
 import { Input, InputField } from "@/components/ui/input";
 import { View, FlatList } from "react-native";
 import { useFonts, Quicksand_400Regular } from "@expo-google-fonts/quicksand";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, ButtonText } from "@/components/ui/button";
 import * as ImagePicker from "expo-image-picker";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "@/components/ui/CustomText";
 import { apiCall } from ".";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Spinner } from "@/components/ui/spinner";
+import { Image } from "react-native";
+import { TouchableOpacity } from "react-native";
+
+const Post = ({
+  user,
+  content,
+  images,
+  likes,
+  likedByCurrentUser,
+  likePost,
+}: {
+  user: { username: string; avatar: string | null };
+  content: string;
+  images: { id: number; url: string }[];
+  likes: number;
+  likedByCurrentUser: boolean;
+  likePost: () => void;
+}) => {
+  return (
+    <View className="mt-2 p-5 m-5 w-[90%] rounded-lg items-center border-[#575A4B]">
+      <View className="flex-row justify-start w-full pb-2 pt-1 border border-[#718355]">
+        {user.avatar ? (
+          <Image
+            source={{ uri: user.avatar }}
+            style={{ width: 30, height: 30, borderRadius: 15 }}
+          />
+        ) : (
+          <Ionicons
+            name="person-circle"
+            size={30}
+            color="#575A4B"
+            className="ml-2.5"
+          />
+        )}
+        <Text className="text-lg mb-3.5 text-center">{user.username}</Text>
+      </View>
+
+      <Text>{content}</Text>
+
+      <View className="w-full items-center my-2.5 border border-[#718335]">
+        {images.map((image) => (
+          <Image
+            key={image.id}
+            source={{ uri: image.url }}
+            className="w-[150px] h-[150px] rounded-lg border border-[#718355]"
+          />
+        ))}
+      </View>
+
+      <View className="flex-row justify-around w-full mt-2.5">
+        <Text>{likes}</Text>
+        <TouchableOpacity onPress={likePost}>
+          <Ionicons
+            name="heart"
+            size={24}
+            color={likedByCurrentUser ? "red" : "#575A4B"}
+            className="mx-5"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Ionicons
+            name="chatbubble"
+            size={24}
+            color="#575A4B"
+            className="mx-5"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const useUser = () => {
   return useQuery({
@@ -23,48 +96,107 @@ const useUser = () => {
   });
 };
 
+const usePosts = () => {
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: () => apiCall("/posts"),
+  });
+};
+
+const useRecipes = () => {
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: () => apiCall("/posts"), //kasnije promijeniti u recipes
+  });
+};
+
 export default function ProfileScreen() {
-  let fonts = useFonts({
-    Quicksand_400Regular,
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => apiCall("/posts"),
+    retry: false,
   });
 
-  const [name, setName] = useState("Ime Prezime");
-  const [bio, setBio] = useState("Enter your bio!");
-  const [isEditingName, setIsEditingName] = useState(false);
-  const [isEditingBio, setIsEditingBio] = useState(false);
-  const [selectedTab, setSelectedTab] = useState("Posts");
+  const { data: user, isLoading, error } = useUser();
+
+  {
+    isLoading && <Spinner />;
+  }
+
+  {
+    error && <Text>Error</Text>;
+  }
+
+  const [userName, setUserName] = useState("");
+  const [bio, setBio] = useState("");
   const [profilePicture, setProfilePicture] = useState<string | null>(null);
+  const [isEditingBio, setIsEditingBio] = useState(false);
+  const [isEditingUserName, setIsEditingUserName] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("Posts");
+  const [originalName, setOriginalName] = useState(userName);
+  const [originalBio, setOriginalBio] = useState(bio);
 
-  const { data, isLoading, error } = useUser();
+  const { data: posts, isLoading: isPostsLoading } = usePosts();
+  const { data: recipes, isLoading: isRecipesLoading } = useRecipes();
 
-  const [posts, setPosts] = useState([
-    { name: "Post 1", id: "1" },
-    { name: "Post 2", id: "2" },
-    { name: "Post 3", id: "3" },
-    { name: "Post 4", id: "4" },
-    { name: "Post 5", id: "5" },
-    { name: "Post 6", id: "6" },
-    { name: "Post 7", id: "7" },
-    { name: "Post 8", id: "8" },
-    { name: "Post 9", id: "9" },
-    { name: "Post 10", id: "10" },
-  ]);
+  interface ProfileUpdateData {
+    username: string;
+    bio: string;
+    profilePicture: string | null;
+  }
 
-  const [recipes, setRecipes] = useState([
-    { name: "Recipe 1", id: "1" },
-    { name: "Recipe 2", id: "2" },
-    { name: "Recipe 3", id: "3" },
-    { name: "Recipe 4", id: "4" },
-    { name: "Recipe 5", id: "5" },
-    { name: "Recipe 6", id: "6" },
-    { name: "Recipe 7", id: "7" },
-    { name: "Recipe 8", id: "8" },
-    { name: "Recipe 9", id: "9" },
-    { name: "Recipe 10", id: "10" },
-  ]);
+  const updateProfileMutation = useMutation<unknown, Error, ProfileUpdateData>({
+    mutationFn: async (updatedData: ProfileUpdateData) => {
+      const [data, status] = await apiCall("/update-profile", {
+        method: "PATCH",
+        body: JSON.stringify(updatedData),
+        headers: { "Content-Type": "application/json" },
+      });
 
-  const toggleEditName = () => setIsEditingName((prev) => !prev);
-  const toggleEditBio = () => setIsEditingBio((prev) => !prev);
+      if (status !== 200) {
+        throw new Error(data.message || "Failed to update profile");
+      }
+
+      return data;
+    },
+    onSuccess: (data) => {
+      console.log("Profile updated successfully:", data);
+    },
+    onError: (error) => {
+      console.log("Error updating profile:", error);
+    },
+  });
+
+  const handleSaveProfile = () => {
+    console.log("Sending data: ", { username: userName, bio, profilePicture });
+    updateProfileMutation.mutate({
+      username: userName,
+      bio: bio,
+      profilePicture: profilePicture,
+    });
+  };
+
+  const toggleEditUserName = () => {
+    setIsEditingUserName((prev) => {
+      if (!prev) {
+        setOriginalName(userName);
+      } else if (userName !== originalName) {
+        handleSaveProfile();
+      }
+      return !prev;
+    });
+  };
+
+  const toggleEditBio = () => {
+    setIsEditingBio((prev) => {
+      if (!prev) {
+        setOriginalBio(bio);
+      } else if (bio !== originalBio) {
+        handleSaveProfile();
+      }
+      return !prev;
+    });
+  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -75,108 +207,179 @@ export default function ProfileScreen() {
 
     if (!result.canceled) {
       setProfilePicture(result.assets[0].uri);
+      handleSaveProfile();
     }
+  };
+
+  const renderPosts = () => {
+    if (isPostsLoading) {
+      return <Spinner />;
+    }
+    if (!posts || posts.length === 0) {
+      return <Text className="text-center text-jet mt-5">No posts yet</Text>;
+    }
+    const userPosts = posts.filter(
+      (post: any) => post.user?.username === user?.username
+    );
+    if (userPosts.length === 0) {
+      return <Text className="text-center text-jet mt-5">No posts yet</Text>;
+    }
+    return (
+      <FlatList
+        className="w-full"
+        keyExtractor={(item) => item.id}
+        data={userPosts}
+        renderItem={({ item }) => (
+          <Post
+            user={item.user}
+            content={item.content}
+            images={item.images}
+            likes={item.likes_count}
+            likedByCurrentUser={item.liked_by_current_user}
+            likePost={() => {
+              apiCall(`/posts/${item.id}/likes`, {
+                method: item.liked_by_current_user ? "DELETE" : "POST",
+              }).then(() => {
+                postsQuery.refetch(); // Refetch posts after liking/unliking
+              });
+            }}
+          />
+        )}
+      />
+    );
+  };
+
+  const renderRecipes = () => {
+    if (isRecipesLoading) {
+      return <Spinner />;
+    }
+    if (!recipes || recipes.length === 0) {
+      return <Text className="text-center text-jet mt-5">No recipes yet</Text>;
+    }
+    const userRecipes = posts.filter(
+      (post: any) => post.user?.username === user?.username
+    );
+    if (userRecipes.length === 0) {
+      return <Text className="text-center text-jet mt-5">No recipes yet</Text>;
+    }
+    return (
+      <FlatList
+        className="w-full"
+        keyExtractor={(item) => item.id}
+        data={userRecipes}
+        renderItem={({ item }) => (
+          <Post
+            user={item.user}
+            content={item.content}
+            images={item.images}
+            likes={item.likes_count}
+            likedByCurrentUser={item.liked_by_current_user}
+            likePost={() => {
+              apiCall(`/posts/${item.id}/likes`, {
+                method: item.liked_by_current_user ? "DELETE" : "POST",
+              }).then(() => {
+                postsQuery.refetch(); // Refetch posts after liking/unliking
+              });
+            }}
+          />
+        )}
+      />
+    );
   };
 
   const renderContent = () => {
-    switch (selectedTab) {
-      case "Posts":
-        return (
-          <FlatList
-            className="w-3/4"
-            keyExtractor={(item) => item.id}
-            data={posts}
-            renderItem={({ item }) => (
-              <Text className="text-jet mt-10 p-10 bg-resedagreen font-quicksand">
-                {item.name}
-              </Text>
-            )}
-          />
-        );
-      case "Recipes":
-        return (
-          <FlatList
-            className="w-3/4"
-            keyExtractor={(item) => item.id}
-            data={recipes}
-            renderItem={({ item }) => (
-              <Text className="text-jet mt-10 p-10 bg-resedagreen font-quicksand">
-                {item.name}
-              </Text>
-            )}
-          />
-        );
-    }
+    return selectedTab === "Posts" ? renderPosts() : renderRecipes();
   };
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (error) {
-    return <Text>Error</Text>;
-  }
+  useEffect(() => {
+    if (user) {
+      setUserName(user.username || "");
+      setBio(user.bio || "");
+      setProfilePicture(user.avatar || null);
+    }
+  }, [user]);
 
   return (
     <View className="flex-1 bg-teagreen">
-      <View className="p-10">
-        <HStack space="xl" className="items-center">
-          <Avatar className="bg-resedagreen">
-            {data.avatar ? (
-              <AvatarImage
-                source={{ uri: data.avatar }}
-                className="w-full h-full"
-              />
-            ) : (
-              <AvatarFallbackText className="font-quicksand">
-                {data.name}
-              </AvatarFallbackText>
-            )}
-          </Avatar>
-          <Ionicons
-            name="create"
-            size={20}
-            onPress={pickImageAsync}
-            className="text-jet"
-          />
-          {isEditingName ? (
-            <Input variant="outline" isDisabled={false} className="flex-1">
+      <View className="pt-10 pl-10 pr-10">
+        <View className="flex-row justify-between items-center">
+          <HStack space="2xl" className="items-center">
+            <Avatar className="bg-resedagreen">
+              {user?.avatar ? (
+                <AvatarImage
+                  source={{ uri: user?.avatar }}
+                  className="w-full h-full"
+                />
+              ) : (
+                <AvatarFallbackText className="font-quicksand">
+                  {user?.username}
+                </AvatarFallbackText>
+              )}
+            </Avatar>
+            <Ionicons
+              name="create"
+              size={20}
+              onPress={pickImageAsync}
+              className="text-jet"
+            />
+          </HStack>
+          {/*Button
+            className="bg-jet rounded-full"
+            //onPress={() => setSelectedTab("Posts")} promijeniti funkcionalnost u logoutanje
+          >
+            <ButtonText className="">Logout</ButtonText>
+          </Button>*/}
+        </View>
+
+        <View className="flex-row mt-5 items-center">
+          {isEditingUserName ? (
+            <Input
+              variant="outline"
+              isDisabled={false}
+              className="flex-1 rounded-full"
+            >
               <InputField
-                onChangeText={setName}
-                placeholder={name}
+                onChangeText={setUserName}
+                placeholder={userName}
                 className="text-jet font-quicksand py-2"
               />
             </Input>
           ) : (
-            <Text className=" text-jet text-2xl font-quicksand">{name}</Text>
+            <Text className=" text-jet text-2xl font-quicksand">
+              {userName}
+            </Text>
           )}
           <Ionicons
             name="create"
             size={20}
-            onPress={toggleEditName}
-            className="text-jet"
+            onPress={toggleEditUserName}
+            className="text-jet ml-5"
           />
-        </HStack>
+        </View>
 
-        <Text className="text-jet text-2xl font-quicksand">
-          {data.first_name}
-        </Text>
-
-        <View className="flex-row mt-10 items-center">
+        <View className="flex-row mt-5 items-center">
           {isEditingBio ? (
-            <Input variant="outline" isDisabled={false} className="flex-1">
+            <Input
+              variant="outline"
+              isDisabled={false}
+              className="flex-1 rounded-full"
+            >
               <InputField
                 onChangeText={setBio}
                 placeholder={bio}
-                className="text-jet font-quicksand py-2"
+                className="text-jet font-quicksand py-1"
               />
             </Input>
           ) : (
-            <Input variant="outline" isDisabled={true} className="flex-1">
+            <Input
+              variant="outline"
+              isDisabled={true}
+              className="flex-1 rounded-full"
+            >
               <InputField
                 onChangeText={setBio}
                 placeholder={bio}
-                className="text-jet font-quicksand py-2"
+                className="text-jet font-quicksand py-1"
               />
             </Input>
           )}
@@ -185,15 +388,15 @@ export default function ProfileScreen() {
             name="create"
             size={20}
             onPress={toggleEditBio}
-            className="text-jet"
+            className="text-jet ml-5"
           />
         </View>
       </View>
 
-      <View className="flex-row mt-4 justify-center">
+      <View className="flex-row mt-5 justify-center">
         {selectedTab === "Posts" ? (
           <Button
-            className="bg-jet mr-3"
+            className="bg-jet mr-3 rounded-full"
             onPress={() => setSelectedTab("Posts")}
           >
             <ButtonText className="">Posts</ButtonText>
@@ -201,7 +404,7 @@ export default function ProfileScreen() {
         ) : (
           <Button
             variant="outline"
-            className="mr-3"
+            className="mr-3 rounded-full"
             onPress={() => setSelectedTab("Posts")}
           >
             <ButtonText className="">Posts</ButtonText>
@@ -210,7 +413,7 @@ export default function ProfileScreen() {
 
         {selectedTab === "Recipes" ? (
           <Button
-            className="bg-jet mr-3"
+            className="bg-jet mr-3 rounded-full"
             onPress={() => setSelectedTab("Recipes")}
           >
             <ButtonText className="">Recipes</ButtonText>
@@ -218,7 +421,7 @@ export default function ProfileScreen() {
         ) : (
           <Button
             variant="outline"
-            className="mr-3"
+            className="mr-3 rounded-full"
             onPress={() => setSelectedTab("Recipes")}
           >
             <ButtonText className="">Recipes</ButtonText>
@@ -226,9 +429,7 @@ export default function ProfileScreen() {
         )}
       </View>
 
-      <View className="flex-1 ml-10 mr-10 mb-10 items-center">
-        {renderContent()}
-      </View>
+      <View className="flex-1 items-center">{renderContent()}</View>
     </View>
   );
 }
