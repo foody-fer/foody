13d63825269f12e71b0d4428aeb93d7471702fe5
sikiDev <karla.sikavica@fer.hr>
2025-01-26 16:56:47 +0100

    progres je potpuno funkcionalan

--- mobile/app/(tabs)/Progress.tsx
+++ mobile/app/(tabs)/Progress.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 
 import {
@@ -17,7 +17,16 @@ import { Text } from "../../components/ui/CustomText";
 import DateTimePicker, {
   DateTimePickerEvent,
 } from "@react-native-community/datetimepicker";
+import { useQuery } from "@tanstack/react-query";
+import { apiCall } from ".";
 type Category = "weight" | "waist" | "thighs" | "hips" | "arms" | "neck";
+import { useMemo } from "react";
+
+interface Measurement {
+  key: string;
+  value: number;
+  recorded_at: string;
+}
 
 const ProgressScreen: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState<Category>("arms");
@@ -26,6 +35,7 @@ const ProgressScreen: React.FC = () => {
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
   const router = useRouter();
+  const [measurements, setMeasurements] = useState<Measurement[]>([]);
 
   const categories: Category[] = [
     "weight",
@@ -54,6 +64,111 @@ const ProgressScreen: React.FC = () => {
       setDate(selectedDate);
     }
   };
+
+  useEffect(() => {
+    fetchLogs();
+  }, [selectedCategory]); // Fetch logs when the selected category changes
+
+  const fetchLogs = async () => {
+    const queryParams = new URLSearchParams({
+      key: selectedCategory,
+    });
+
+    try {
+      const response = await apiCall(
+        `/measurements.json?${queryParams.toString()}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response) {
+        setMeasurements(response);
+
+        console.log("Measurements fetched successfully:", response);
+      }
+    } catch (error) {
+      console.error("Error fetching measurements:", error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const data = {
+      measurement: {
+        key: selectedCategory,
+        value: value,
+        recorded_at: date,
+      },
+    };
+
+    try {
+      const response = await apiCall(`/measurements`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setModalVisible(false);
+      await fetchLogs();
+    } catch (error) {
+      setModalVisible(false);
+      console.error("Doslo je do greske:", error);
+    }
+  };
+
+  const formatDate = (recorded_at: string): string => {
+    // Replace "UTC" with "Z" to make it compatible with the Date constructor
+    const utcFormatted = recorded_at.replace(" UTC", "Z");
+    const date = new Date(utcFormatted);
+
+    // Extract date components
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    // Return formatted date
+    return `${day}.${month}.${year}, ${hours}:${minutes}`;
+  };
+
+  const getChartData = (
+    measurements: Measurement[],
+    selectedCategory: Category
+  ) => {
+    const filteredData = measurements.filter(
+      (entry) => entry.key === selectedCategory
+    );
+    console.log("Sorted data for chart:", filteredData); // Debugging
+
+    const labels = filteredData.map((entry) => {
+      const date = new Date(entry.recorded_at.replace(" UTC", "Z"));
+      if (isNaN(date.getTime())) {
+        console.warn(`Invalid date format: ${entry.recorded_at}`);
+        return "Invalid Date";
+      }
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      return `${day}.${month}`; // Format datuma: dd.mm
+    });
+
+    const data = filteredData.map((entry) => entry.value);
+
+    return {
+      labels: labels.reverse(),
+      datasets: [{ data: data.reverse() }],
+    };
+  };
+
+  const chartData = useMemo(
+    () => getChartData(measurements, selectedCategory),
+    [measurements, selectedCategory]
+  );
 
   return (
     <SafeAreaView className="flex-1 bg-[#CFE1B9]">
@@ -103,27 +218,31 @@ const ProgressScreen: React.FC = () => {
         {/* Graph Section */}
         <View className="h-[40%] items-center justify-center">
           <View className="bg-white p-4 rounded-xl shadow-lg">
-            <LineChart
-              data={{
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-                datasets: [
-                  {
-                    data: [30, 37, 40, 69, 70, 45, 90],
-                  },
-                ],
-              }}
-              width={300}
-              height={200}
-              chartConfig={{
-                backgroundColor: "#FFFFFF",
-                backgroundGradientFrom: "#FFFFFF",
-                backgroundGradientTo: "#FFFFFF",
-                decimalPlaces: 0,
-                color: (opacity = 1) => `rgba(113, 131, 85, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(55, 55, 55, ${opacity})`,
-              }}
-              style={{ borderRadius: 16 }}
-            />
+            {chartData.datasets[0].data.length > 0 ? (
+              <LineChart
+                data={chartData}
+                width={300}
+                height={200}
+                chartConfig={{
+                  backgroundColor: "#FFFFFF",
+                  backgroundGradientFrom: "#FFFFFF",
+                  backgroundGradientTo: "#FFFFFF",
+                  decimalPlaces: 0,
+                  color: (opacity = 1) => `rgba(113, 131, 85, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(55, 55, 55, ${opacity})`,
+                }}
+                style={{
+                  borderRadius: 18,
+                  alignSelf: "center",
+                  marginLeft: -10, // Korigira marginu (ako je potrebno)
+                  marginRight: -10,
+                }}
+              />
+            ) : (
+              <Text className="text-[#575A4B] text-lg text-center">
+                No data available for this category.
+              </Text>
+            )}
           </View>
         </View>
 
@@ -138,23 +257,32 @@ const ProgressScreen: React.FC = () => {
             showsVerticalScrollIndicator={false}
           >
             <View className="bg-white w-[130px] py-4 rounded-lg items-center">
-              {[
-                { value: "24 cm", date: "15.3.2025" },
-                { value: "30 cm", date: "15.8.2025" },
-                { value: "31 cm", date: "16.9.2025" },
-              ].map((entry, index) => (
-                <View key={index} className="flex-row items-center mb-2">
-                  <View className="w-[10px] h-[10px] bg-[#718355] rounded-full mr-2" />
-                  <View>
-                    <Text className="text-[18px] font-bold text-[#373737]">
-                      {entry.value}
-                    </Text>
-                    <Text className="text-[12px] text-[#575A4B]">
-                      {entry.date}
-                    </Text>
+              {measurements
+                .filter((entry) => entry.key === selectedCategory) // Filter entries based on the selected category
+                .map((entry, index) => (
+                  <View key={index} className="flex-row items-center mb-2 px-8">
+                    <View className="w-[10px] h-[10px] bg-[#718355] rounded-full mr-2" />
+                    <View>
+                      <Text className="text-[18px] font-bold text-[#373737]">
+                        {entry.value}
+                        {selectedCategory === "weight" ? (
+                          <Text> kg</Text>
+                        ) : (
+                          <Text> cm</Text>
+                        )}
+                      </Text>
+                      <Text className="text-[12px] text-[#575A4B]">
+                        {formatDate(entry.recorded_at)}{" "}
+                      </Text>
+                    </View>
                   </View>
-                </View>
-              ))}
+                ))}
+              {measurements.filter((entry) => entry.key === selectedCategory)
+                .length === 0 && (
+                <Text className="text-[#575A4B] text-center text-[16px] px-4 py-2">
+                  Log progress to see it displayed.
+                </Text>
+              )}
             </View>
           </ScrollView>
         </View>
@@ -212,7 +340,7 @@ const ProgressScreen: React.FC = () => {
 
               <TouchableOpacity
                 className="bg-[#718355] py-2 px-4 rounded-full items-center w-3/4 self-center"
-                onPress={() => setModalVisible(false)}
+                onPress={handleSubmit}
               >
                 <Text className="text-white font-bold text-lg">
                   Submit Progress
