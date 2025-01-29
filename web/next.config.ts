5671679eb00f1dbf34879aa39279e652acd754b1
Fran Zekan <zekan.fran369@gmail.com>
2025-01-29 21:15:52 +0100

    fixes and deployment

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
+const getFrontendUrl = () => {
+  if (process.env.VERCEL_URL) {
+    if (process.env.VERCEL_ENV === "production") {
+      return `https://foody.zeko.run`;
+    }
+
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT || 3000}`;
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_FRONTEND_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:${process.env.PORT || 3000}`,
+    NEXT_PUBLIC_FRONTEND_URL: getFrontendUrl(),
   },
 };
 
